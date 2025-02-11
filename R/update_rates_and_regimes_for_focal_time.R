59a231e1576b1d70d82e06b4b5b07f78a17bafe6
Maël DORÉ <mael.dore@gmail.com>
2025-02-11 14:44:57 +0100

    :paperclip: Register C scripts in deepSRAPP package

--- R/update_rates_and_regimes_for_focal_time.R
+++ R/update_rates_and_regimes_for_focal_time.R
@@ -539,13 +539,15 @@ update_rates_and_regimes_for_focal_time <- function (BAMM_object, focal_time,
 # Source: BAMMtools:::getRecursiveSequence()
 # Authors: Dan Rabosky, Mike Grundler
 
+#' @useDynLib deepSTRAPP
+
 getRecursiveSequence <- function (phy)
 {
   rootnd = as.integer(phy$Nnode + 2)
   anc = as.integer(phy$edge[, 1])
   desc = as.integer(phy$edge[, 2])
   ne = as.integer(dim(phy$edge)[1])
-  L = .C("setrecursivesequence", anc, desc, rootnd, ne, integer(ne + 1), integer(ne + 1), PACKAGE = "BAMMtools")
+  L = .C("setrecursivesequence", anc, desc, rootnd, ne, integer(ne + 1), integer(ne + 1), PACKAGE = "deepSTRAPP")
   phy$downseq = as.integer(L[[5]])
   phy$lastvisit = as.integer(L[[6]])
   return(phy)
