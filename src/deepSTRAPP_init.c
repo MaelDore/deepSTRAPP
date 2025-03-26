4ae58100e3c0b5999057e44091176c50a28cb4db
Maël DORÉ <mael.dore@gmail.com>
2025-03-26 20:24:29 +0100

    :game_die: Add stuff for compatibility with Rcpp

--- src/deepSTRAPP_init.c
+++ src/deepSTRAPP_init.c
@@ -1,3 +1,5 @@
+#include <R.h>
+#include <Rinternals.h>
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
@@ -8,13 +10,21 @@
 /* .C calls */
 extern void setrecursivesequence(void *, void *, void *, void *, void *, void *);
 
+/* .Call calls */
+extern SEXP cache_descendants(SEXP);
+
 static const R_CMethodDef CEntries[] = {
     {"setrecursivesequence", (DL_FUNC) &setrecursivesequence, 6},
     {NULL, NULL, 0}
 };
 
+static const R_CallMethodDef CallEntries[] = {
+    {"cache_descendants",   (DL_FUNC) &cache_descendants,   1},
+    {NULL, NULL, 0}
+};
+
 void R_init_deepSTRAPP(DllInfo *dll)
 {
-    R_registerRoutines(dll, CEntries, NULL, NULL, NULL);
+    R_registerRoutines(dll, CEntries, CallEntries, NULL, NULL);
     R_useDynamicSymbols(dll, FALSE);
 }
