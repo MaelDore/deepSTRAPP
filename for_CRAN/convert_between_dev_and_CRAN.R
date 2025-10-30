270f69400a2221b2e5d4c4fd4bb2a6718ec474a5
Maël DORÉ <mael.dore@gmail.com>
2025-10-30 18:53:34 +0100

    :policeman: Add folder to store objects not compatible with CRAN

---
+++ for_CRAN/convert_between_dev_and_CRAN.R
@@ -0,0 +1,55 @@
+
+##### Script to convert package between development versions and CRAN releases #####
+
+### Development versions
+
+# Contain all datasets, including deepSTRAPP outputs for all four types of data
+# Include doc for all datasets
+# Produce vignette visual outputs using code evaluation
+# Run all examples including loading of deepSTRAPP outputs
+
+### CRAN Releases ###
+
+# Needs to reduce size to comply with CRAN policies
+# Do not contain datasets of deepSTRAPP outputs (only for binary data as used in the Main tutorial)
+# Do not include doc for deepSTRAPP datasets (only for binary data as used in the Main tutorial)
+# Produce vignette visual outputs based on pre-rendered PNG images
+# Do not run examples including loading of deepSTRAPP outputs
+
+### 1/ From development version to CRAN release ####
+
+## 1.1/ Remove deepSTRAPP datasets from data
+unlink(x = "./data/Ponerinae_deepSTRAPP_cont_old_calib_0_40.rda", force = T)
+unlink(x = "./data/Ponerinae_deepSTRAPP_cat_3lvl_old_calib_0_40.rda", force = T)
+unlink(x = "./data/Ponerinae_deepSTRAPP_biogeo_old_calib_0_40.rda", force = T)
+
+## 1.2/ Replace datasets documentation
+unlink(x = "./R/datasets_doc.R", force = T)
+file.copy(from = "./for_CRAN/datasets_doc_for_CRAN.R", to = "./R/datasets_doc_for_CRAN.R", overwrite = T)
+
+## 1.3/ Add pre-rendered visual outputs to the vignette folder
+prerendered_vignette_outputs_path <- list.files(path = "./for_CRAN/", pattern = ".PNG")
+file.copy(from = paste0("./for_CRAN/",prerendered_vignette_outputs_path), to = paste0("./vignettes/figures/",prerendered_vignette_outputs_path), overwrite = T)
+
+## 1.4/ Rerun check once done
+devtools::check()
+
+
+### 2/ From CRAN release to development version ####
+
+## 2.1/ Add deepSTRAPP datasets to data
+file.copy(from = "./for_CRAN/Ponerinae_deepSTRAPP_cont_old_calib_0_40.rda", to = "./data/Ponerinae_deepSTRAPP_cont_old_calib_0_40.rda", overwrite = T)
+file.copy(from = "./for_CRAN/Ponerinae_deepSTRAPP_cat_3lvl_old_calib_0_40.rda", to = "./data/Ponerinae_deepSTRAPP_cat_3lvl_old_calib_0_40.rda", overwrite = T)
+file.copy(from = "./for_CRAN/Ponerinae_deepSTRAPP_biogeo_old_calib_0_40.rda", to = "./data/Ponerinae_deepSTRAPP_biogeo_old_calib_0_40.rda", overwrite = T)
+
+## 2.2/ Replace datasets documentation
+unlink(x = "./R/datasets_doc_for_CRAN.R", force = T)
+file.copy(from = "./for_CRAN/datasets_doc.R", to = "./R/datasets_doc.R", overwrite = T)
+
+## 2.3/ Remove pre-rendered visual outputs to the vignette folder
+prerendered_vignette_outputs_path <- list.files(path = "./for_CRAN/", pattern = ".PNG")
+unlink(x = paste0("./vignettes/figures/",prerendered_vignette_outputs_path), force = T)
+
+## 2.4/ Rerun check once done
+devtools::check()
+
